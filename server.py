8b085402d9b1df936e0b456830ef84291561e550
burnsed <rburns@stic.net>
2019-02-20 13:24:44 -0800

    catch errors

--- server.py
+++ server.py
@@ -45,88 +45,92 @@ class Bridge(BaseHTTPRequestHandler):
         self.finish()
 
     def do_POST(self):
-        log.info("POST received: " + self.path)
-        path = urlparse.urlparse(self.path)
-        parts = os.path.split(path.path)
-        log.info("Parts: %s"%str(parts))
-        if len(parts) != 2:
-            log.info("Invalid path length: %s"%path.path)
-            self._respond(400, "Invalid path")
-            return
+        try:
+            log.info("POST received: " + self.path)
+            path = urlparse.urlparse(self.path)
+            parts = os.path.split(path.path)
+            log.info("Parts: %s"%str(parts))
+            if len(parts) != 2:
+                log.info("Invalid path length: %s"%path.path)
+                self._respond(400, "Invalid path")
+                return
 
-        if parts[0].strip('/') != "twilio":
-            log.info("Not a twilio request: %s"%path.path)
-            self._respond(404, "not a twilio path")
-            return
+            if parts[0].strip('/') != "twilio":
+                log.info("Not a twilio request: %s"%path.path)
+                self._respond(404, "not a twilio path")
+                return
 
-        # reload config
-        self.config = configparser.ConfigParser()
-        self.config.read(CONFIG_FILE)
+            # reload config
+            self.config = configparser.ConfigParser()
+            self.config.read(CONFIG_FILE)
 
-        flow = parts[1]
-        flow_data = {}
-        if flow in self.config:
-            self.log.info("Alert received for flow '%s'" % flow)
-            if all(k in self.config[flow] for k in ("sid", "From")):
-                flow_data["sid"] = self.config[flow]["sid"]
-                flow_data["From"] = self.config[flow]["From"]
+            flow = parts[1]
+            flow_data = {}
+            if flow in self.config:
+                self.log.info("Alert received for flow '%s'" % flow)
+                if all(k in self.config[flow] for k in ("sid", "From")):
+                    flow_data["sid"] = self.config[flow]["sid"]
+                    flow_data["From"] = self.config[flow]["From"]
+                else:
+                    self.log.error("Flow '%s' is missing parameters in the config"%flow)
+                    self._respond(500, "flow is missing parameters in the server config")
+                    return
             else:
-                self.log.error("Flow '%s' is missing parameters in the config"%flow)
-                self._respond(500, "flow is missing parameters in the server config")
-                return
-        else:
-            self.log.info("Alert recieved for unknown flow '%s'"%flow)
-            self._respond(404, "unknown flow")
-            return
-
-        # At this point there is a valid populated twilio flow
-        twilio_url = TWILIO_FLOW_URL.format(**flow_data)
-        self.log.info("Trigger Twilio flow: %s"%twilio_url)
-        content_length = int(self.headers['Content-Length'])
-        post_data_str = self.rfile.read(content_length)
-        post_data = json.loads(post_data_str)
-        if "evalMatches" in post_data:
-            del(post_data["evalMatches"])
-
-        r = requests.post("https://api.keyvalue.xyz/new/%s"%flow)
-        if r.status_code != 200:
-            self.log.error("Keyvalue create failed: %d, %s" %
-                           (r.status_code, r.reason))
-            self._respond(500, "keyvalue creation failed: " + r.reason)
-            return
-
-        post_data["status_path"] = r.content.strip().replace(
-                "https://api.keyvalue.xyz/", "")
-
-        r = requests.post("https://api.keyvalue.xyz/%s/%s"%(post_data["status_path"], "pending"))
-        if r.status_code != 200:
-            self.log.error("Keyvalue set failed: %d, %s" %
-                           (r.status_code, r.reason))
-            self._respond(500, "keyvalue set: " + r.reason)
-            return
-
-        recipient_conf = flow+".recipients"
-        if not recipient_conf in self.config:
-            self.log.error(
-                "Flow '%s' is missing the .recipients in the config" % flow)
-            self._respond(500, "flow has no recipients")
-            return
-
-        for name, number in self.config[recipient_conf].items():
-            post_data["person"] = name
-            params = {"To": number, "From": flow_data["From"], "Parameters": json.dumps(post_data)}
-            self.log.info("Parameters: %s" % str(params))
-            resp = requests.post(twilio_url, data=params, auth=(self.twilio_sid, self.twilio_auth_token))
-            self.log.info("URL: %s"%(resp.url))
-            if resp.status_code != 200:
-                self.log.error("Twilio POST failed: %d, %s"%(resp.status_code, resp.reason))
-                self._respond(500, "Twilio post failed: " + resp.reason)
+                self.log.info("Alert recieved for unknown flow '%s'"%flow)
+                self._respond(404, "unknown flow")
                 return
 
-        # All done
-        self.log.info("Twilio POST SUCCEEDED!")
-        self.respond(200, "success")
-        return
+            # At this point there is a valid populated twilio flow
+            twilio_url = TWILIO_FLOW_URL.format(**flow_data)
+            self.log.info("Trigger Twilio flow: %s"%twilio_url)
+            content_length = int(self.headers['Content-Length'])
+            post_data_str = self.rfile.read(content_length)
+            post_data = json.loads(post_data_str)
+            if "evalMatches" in post_data:
+                del(post_data["evalMatches"])
+
+            r = requests.post("https://api.keyvalue.xyz/new/%s"%flow)
+            if r.status_code != 200:
+                self.log.error("Keyvalue create failed: %d, %s" %
+                            (r.status_code, r.reason))
+                self._respond(500, "keyvalue creation failed: " + r.reason)
+                return
+
+            post_data["status_path"] = r.content.strip().replace(
+                    "https://api.keyvalue.xyz/", "")
+
+            r = requests.post("https://api.keyvalue.xyz/%s/%s"%(post_data["status_path"], "pending"))
+            if r.status_code != 200:
+                self.log.error("Keyvalue set failed: %d, %s" %
+                            (r.status_code, r.reason))
+                self._respond(500, "keyvalue set: " + r.reason)
+                return
+
+            recipient_conf = flow+".recipients"
+            if not recipient_conf in self.config:
+                self.log.error(
+                    "Flow '%s' is missing the .recipients in the config" % flow)
+                self._respond(500, "flow has no recipients")
+                return
+
+            for name, number in self.config[recipient_conf].items():
+                post_data["person"] = name
+                params = {"To": number, "From": flow_data["From"], "Parameters": json.dumps(post_data)}
+                self.log.info("Parameters: %s" % str(params))
+                resp = requests.post(twilio_url, data=params, auth=(self.twilio_sid, self.twilio_auth_token))
+                self.log.info("URL: %s"%(resp.url))
+                if resp.status_code != 200:
+                    self.log.error("Twilio POST failed: %d, %s"%(resp.status_code, resp.reason))
+                    self._respond(500, "Twilio post failed: " + resp.reason)
+                    return
+
+            # All done
+            self.log.info("Twilio POST SUCCEEDED!")
+            self.respond(200, "success")
+            return
+        except Exception as e:
+            self.log.error("Exception in do_POST: " + str(e))
+            self.respond(500, "Internal exception")
 
 def run(log, port=8080):
     server_address = ('127.0.0.1', port)
